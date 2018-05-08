f17c9c5aac3b061b39e529d3ed26b85ffb006436
Andrew Berry <deviantintegral@gmail.com>
2018-05-08 11:51:20 -0400

    Refactor out a private method

--- test_modules/media_mpx_test/src/JsonResponse.php
+++ test_modules/media_mpx_test/src/JsonResponse.php
@@ -19,20 +19,34 @@ class JsonResponse extends Response {
    */
   public function __construct($status = 200, array $headers = [], $body = NULL, $version = '1.1', $reason = NULL) {
     if (isset($body)) {
-      if (is_string($body) && is_file($body)) {
-        $body = fopen($body, 'r');
-      }
-      elseif (is_string($body) && is_file(self::TESTS_FIXTURES . $body)) {
-        $body = fopen(self::TESTS_FIXTURES . $body, 'r');
-      }
-      elseif (is_array($body)) {
-        $body = \GuzzleHttp\json_encode($body);
-      }
+      $body = $this->generateBody($body);
     }
     $headers += [
       'Content-Type' => 'application/json',
     ];
     parent::__construct($status, $headers, $body, $version, $reason);
+  }
+
+  /**
+   * Generate the body from a path, string, or array.
+   *
+   * @param string|array $body
+   *   The mock body.
+   *
+   * @return bool|resource|string
+   *   The body resource or string.
+   */
+  private function generateBody($body) {
+    if (is_string($body) && is_file($body)) {
+      $body = fopen($body, 'r');
+    }
+    elseif (is_string($body) && is_file(self::TESTS_FIXTURES . $body)) {
+      $body = fopen(self::TESTS_FIXTURES . $body, 'r');
+    }
+    elseif (is_array($body)) {
+      $body = \GuzzleHttp\json_encode($body);
+    }
+    return $body;
   }
 
 }
