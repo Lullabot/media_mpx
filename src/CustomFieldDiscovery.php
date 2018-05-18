19727bf58a4061a48440a74c3d1a2b82f6930e9e
Andrew Berry <deviantintegral@gmail.com>
2018-05-18 16:21:29 -0400

    Break down the giant getCustomFields() method

--- src/CustomFieldDiscovery.php
+++ src/CustomFieldDiscovery.php
@@ -67,55 +67,7 @@ class CustomFieldDiscovery implements CustomFieldDiscoveryInterface {
 
     // Search for classes within all PSR-0 namespace locations.
     foreach ($this->getPluginNamespaces() as $namespace => $dirs) {
-      foreach ($dirs as $dir) {
-        if (file_exists($dir)) {
-          $iterator = new \RecursiveIteratorIterator(
-            new \RecursiveDirectoryIterator($dir, \RecursiveDirectoryIterator::SKIP_DOTS)
-          );
-          foreach ($iterator as $fileinfo) {
-            if ($fileinfo->getExtension() == 'php') {
-              if ($cached = $this->fileCache->get($fileinfo->getPathName())) {
-                if (isset($cached['namespace'])) {
-                  // Explicitly unserialize this to create a new object instance.
-                  /** @var \Lullabot\Mpx\DataService\DiscoveredCustomField $discovered */
-                  $discovered = unserialize($cached['content']);
-                  $definitions[$cached['service']][$cached['objectType']][$cached['namespace']] = $discovered;
-                }
-                continue;
-              }
-
-              $sub_path = $iterator->getSubIterator()->getSubPath();
-              $sub_path = $sub_path ? str_replace(DIRECTORY_SEPARATOR, '\\', $sub_path) . '\\' : '';
-              $class = $namespace . '\\' . $sub_path . $fileinfo->getBasename('.php');
-
-              // The filename is already known, so there is no need to find the
-              // file. However, StaticReflectionParser needs a finder, so use a
-              // mock version.
-              $finder = MockFileFinder::create($fileinfo->getPathName());
-              $parser = new StaticReflectionParser($class, $finder, TRUE);
-
-              /** @var $annotation \Lullabot\Mpx\DataService\Annotation\CustomField */
-              if ($annotation = $reader->getClassAnnotation($parser->getReflectionClass(), CustomField::class)) {
-                if (!is_subclass_of($class, CustomFieldInterface::class)) {
-                  throw new \RuntimeException(sprintf('%s must implement %s.', $class, CustomFieldInterface::class));
-                }
-
-                $discovered = new DiscoveredCustomField(
-                  $class, $annotation
-                );
-                $definitions[$annotation->service][$annotation->objectType][$annotation->namespace] = $discovered;
-
-                // Explicitly serialize this to create a new object instance.
-                $this->fileCache->set($fileinfo->getPathName(), ['service' => $annotation->service, 'objectType' => $annotation->objectType, 'namespace' => $annotation->namespace, 'content' => serialize($discovered)]);
-              }
-              else {
-                // Store a NULL object, so the file is not reparsed again.
-                $this->fileCache->set($fileinfo->getPathName(), [NULL]);
-              }
-            }
-          }
-        }
-      }
+      $this->getDefinitions($dirs, $definitions, $namespace, $reader);
     }
 
     // Don't let annotation loaders pile up.
@@ -147,6 +99,104 @@ class CustomFieldDiscovery implements CustomFieldDiscoveryInterface {
     }
 
     return $plugin_namespaces;
+  }
+
+  /**
+   * @param $dirs
+   * @param $definitions
+   * @param $namespace
+   * @param $reader
+   *
+   * @return mixed
+   */
+  private function getDefinitions($dirs, &$definitions, $namespace, $reader) {
+    foreach ($dirs as $dir) {
+      if (file_exists($dir)) {
+        $this->fetchFromDirectory($definitions, $namespace, $reader, $dir);
+      }
+    }
+  }
+
+  /**
+   * @param $definitions
+   * @param $namespace
+   * @param $reader
+   * @param $dir
+   */
+  private function fetchFromDirectory(&$definitions, $namespace, $reader, $dir) {
+    $iterator = new \RecursiveIteratorIterator(
+      new \RecursiveDirectoryIterator($dir, \RecursiveDirectoryIterator::SKIP_DOTS)
+    );
+    foreach ($iterator as $fileinfo) {
+      if ($fileinfo->getExtension() == 'php') {
+        $this->fetchFromFile($definitions, $namespace, $reader, $fileinfo, $iterator);
+      }
+    }
+  }
+
+  /**
+   * @param $definitions
+   * @param $namespace
+   * @param $reader
+   * @param $fileinfo
+   * @param $iterator
+   */
+  private function fetchFromFile(&$definitions, $namespace, $reader, $fileinfo, $iterator): void {
+    if ($cached = $this->fileCache->get($fileinfo->getPathName())) {
+      if (isset($cached['namespace'])) {
+        // Explicitly unserialize this to create a new object instance.
+        /** @var \Lullabot\Mpx\DataService\DiscoveredCustomField $discovered */
+        $discovered = unserialize($cached['content']);
+        $definitions[$cached['service']][$cached['objectType']][$cached['namespace']] = $discovered;
+        return;
+      }
+    }
+
+    $class = $this->parseClassName($namespace, $fileinfo, $iterator);
+
+    // The filename is already known, so there is no need to find the
+    // file. However, StaticReflectionParser needs a finder, so use a
+    // mock version.
+    $finder = MockFileFinder::create($fileinfo->getPathName());
+    $parser = new StaticReflectionParser($class, $finder, TRUE);
+
+    /** @var $annotation \Lullabot\Mpx\DataService\Annotation\CustomField */
+    if (!$annotation = $reader->getClassAnnotation($parser->getReflectionClass(), CustomField::class)) {
+      // Store a NULL object, so the file is not reparsed again.
+      $this->fileCache->set($fileinfo->getPathName(), [NULL]);
+      return;
+    }
+
+    if (!is_subclass_of($class, CustomFieldInterface::class)) {
+      throw new \RuntimeException(sprintf('%s must implement %s.', $class, CustomFieldInterface::class));
+    }
+
+    $discovered = new DiscoveredCustomField(
+      $class, $annotation
+    );
+    $definitions[$annotation->service][$annotation->objectType][$annotation->namespace] = $discovered;
+
+    // Explicitly serialize this to create a new object instance.
+    $this->fileCache->set($fileinfo->getPathName(), [
+      'service' => $annotation->service,
+      'objectType' => $annotation->objectType,
+      'namespace' => $annotation->namespace,
+      'content' => serialize($discovered)
+    ]);
+  }
+
+  /**
+   * @param $namespace
+   * @param $fileinfo
+   * @param $iterator
+   *
+   * @return string
+   */
+  private function parseClassName($namespace, $fileinfo, $iterator): string {
+    $sub_path = $iterator->getSubIterator()->getSubPath();
+    $sub_path = $sub_path ? str_replace(DIRECTORY_SEPARATOR, '\\', $sub_path) . '\\' : '';
+    $class = $namespace . '\\' . $sub_path . $fileinfo->getBasename('.php');
+    return $class;
   }
 
 }
