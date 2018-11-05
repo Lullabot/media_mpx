9bf4b226fa6ca0f0f26c5c4b61d5ecd6c33c00fe
Andrew Berry <deviantintegral@gmail.com>
2018-11-05 09:57:25 -0500

    Extend from the existing iframe

--- src/Element/MediaMpxIframeWrapper.php
+++ src/Element/MediaMpxIframeWrapper.php
@@ -2,45 +2,20 @@
 
 namespace Drupal\media_mpx\Element;
 
-use Drupal\Core\Render\Element\RenderElement;
-use Drupal\Core\Template\Attribute;
-
 /**
  * Providers an element design for wrapping media iframes.
  *
  * @RenderElement("media_mpx_iframe_wrapper")
  */
-class MediaMpxIframeWrapper extends RenderElement {
+class MediaMpxIframeWrapper extends MediaMpxIframe {
 
   /**
    * {@inheritdoc}
    */
   public function getInfo() {
-    return [
-      '#theme' => 'media_mpx_iframe_wrapper',
-      '#url' => '',
-      '#attributes' => [],
-      '#pre_render' => [
-        [static::class, 'preRenderInlineFrameEmbed'],
-      ],
-    ];
-  }
-
-  /**
-   * Transform the render element structure into a renderable one.
-   *
-   * @param array $element
-   *   An element array before being processed.
-   *
-   * @return array
-   *   The processed and renderable element.
-   */
-  public static function preRenderInlineFrameEmbed(array $element) {
-    if (is_array($element['#attributes'])) {
-      $element['#attributes'] = new Attribute($element['#attributes']);
-    }
-
-    return $element;
+    $info = parent::getInfo();
+    $info['#theme'] = 'media_mpx_iframe_wrapper';
+    return $info;
   }
 
 }
