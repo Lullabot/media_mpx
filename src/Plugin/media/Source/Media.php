b7546bc581b090867a0918a81975c54048ef50a0
Matthew Oliveira <m4olivei@gmail.com>
2019-03-06 10:04:50 -0500

    Issue #112 Adjustment to comment for readability

--- src/Plugin/media/Source/Media.php
+++ src/Plugin/media/Source/Media.php
@@ -620,8 +620,8 @@ class Media extends MediaSourceBase implements MediaSourceInterface {
     $bundles = $this->entityTypeManager->getStorage('media_type')->loadMultiple();
 
     $bundles = array_filter($bundles, function (MediaType $bundle) {
-      // Filter out any bundles that are use the same source plugin. That would
-      // be silly.
+      // Filter out any bundles that use this source plugin. That would be
+      // silly.
       if ($bundle->getSource()->getPluginId() === $this->getPluginId()) {
         return FALSE;
       }
