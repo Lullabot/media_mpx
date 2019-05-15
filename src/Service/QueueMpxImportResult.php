255b189f22da4dd99dd41fdb615fa00b18680f58
Salvador Molina <salva.momo@gmail.com>
2019-05-15 13:09:30 +0200

    Improve QueueVideoImports response objects with more meaningful data

---
+++ src/Service/QueueMpxImportResult.php
@@ -0,0 +1,63 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Drupal\media_mpx\Service;
+
+use Lullabot\Mpx\DataService\Media\Media;
+
+/**
+ * Holds the result of trying to queue an mpx item for later import.
+ *
+ * @package Drupal\media_mpx\Service
+ */
+class QueueMpxImportResult {
+
+  /**
+   * Whether the queueing attempt was succesful.
+   *
+   * @var bool
+   */
+  private $success;
+
+  /**
+   * The mpx Media item.
+   *
+   * @var \Lullabot\Mpx\DataService\Media\Media
+   */
+  private $mpxMediaItem;
+
+  /**
+   * QueueMpxImportResult constructor.
+   *
+   * @param bool $success
+   *   Whether the queueing attempt was succesful or not.
+   * @param \Lullabot\Mpx\DataService\Media\Media $mpx_media
+   *   The mpx Media item.
+   */
+  public function __construct(bool $success, Media $mpx_media) {
+    $this->success = $success;
+    $this->mpxMediaItem = $mpx_media;
+  }
+
+  /**
+   * Returns whether the queuing attempt was succesful or not.
+   *
+   * @return bool
+   *   TRUE if the item was queued for later import. FALSE otherwise.
+   */
+  public function wasSuccesful(): bool {
+    return $this->success;
+  }
+
+  /**
+   * Returns the mpx Media item for which the queue attempt was done.
+   *
+   * @return \Lullabot\Mpx\DataService\Media\Media
+   *   The mpx Media item.
+   */
+  public function getMpxMediaItem(): Media {
+    return $this->mpxMediaItem;
+  }
+
+}
