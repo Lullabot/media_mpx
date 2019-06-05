bafec1ca5171800ec9d8f79adc72d8f099b6154c
GERMAN MONTES <german.montes.m@gmail.com>
2019-06-05 16:16:24 -0500

    feature: on update process check for the data on mpx and create it if is necessary

--- src/Form/UpdateMediaItemForVideoType.php
+++ src/Form/UpdateMediaItemForVideoType.php
@@ -8,10 +8,16 @@ use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\media\Entity\Media;
+use Drupal\media\MediaTypeInterface;
+use Drupal\media_mpx\DataObjectFactoryCreator;
+use Drupal\media_mpx\DataObjectImporter;
 use Drupal\media_mpx\MpxLogger;
 use Drupal\media_mpx\Repository\MpxMediaType;
 use Drupal\media_mpx\Service\UpdateVideoItem\UpdateVideoItem;
 use Drupal\media_mpx\Service\UpdateVideoItem\UpdateVideoItemRequest;
+use Lullabot\Mpx\DataService\ByFields;
+use Lullabot\Mpx\DataService\ObjectListQuery;
+use Lullabot\Mpx\DataService\Media\Media as MpxMedia;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
@@ -50,13 +56,21 @@ class UpdateMediaItemForVideoType extends FormBase {
   private $logger;
 
   /**
+   * The Data Object Factory Creator.
+   *
+   * @var \Drupal\media_mpx\DataObjectFactoryCreator
+   */
+  private $dataObjectFactoryCreator;
+
+  /**
    * UpdateMediaItemForAccount constructor.
    */
-  public function __construct(EntityTypeManagerInterface $entityTypeManager, UpdateVideoItem $updateVideoItem, MpxMediaType $mpxTypeRepository, MpxLogger $logger) {
+  public function __construct(EntityTypeManagerInterface $entityTypeManager, UpdateVideoItem $updateVideoItem, MpxMediaType $mpxTypeRepository, MpxLogger $logger, DataObjectFactoryCreator $dataObjectFactoryCreator) {
     $this->mediaStorage = $entityTypeManager->getStorage('media');
     $this->updateVideoItemService = $updateVideoItem;
     $this->mpxTypeRepository = $mpxTypeRepository;
     $this->logger = $logger;
+    $this->dataObjectFactoryCreator = $dataObjectFactoryCreator;
   }
 
   /**
@@ -67,7 +81,8 @@ class UpdateMediaItemForVideoType extends FormBase {
       $container->get('entity_type.manager'),
       $container->get('media_mpx.service.update_video_item'),
       $container->get('media_mpx.repository.mpx_media_types'),
-      $container->get('media_mpx.exception_logger')
+      $container->get('media_mpx.exception_logger'),
+      $container->get('media_mpx.data_object_factory_creator')
     );
   }
 
@@ -108,22 +123,34 @@ class UpdateMediaItemForVideoType extends FormBase {
   public function submitForm(array &$form, FormStateInterface $form_state) {
     $video_type = $form_state->getValue('video_type');
     $guid = $form_state->getValue('guid');
-
+    $video_item = NULL;
     if (!$video_item = $this->loadVideoMatchingGuidAndType($guid, $video_type)) {
-      $this->messenger()->addError($this->t('There are no video items with the selected GUID and Type.'));
-      return;
+      $videoMpxData = $this->loadVideoMatchingGuidAndTypeFromMpx($guid, $video_type);
+      $mpx_id = (int) end(explode('/', (string) $videoMpxData->getId()));
+      $request = new UpdateVideoItemRequest($mpx_id, $video_type);
+      try {
+        $this->updateVideoItemService->execute($request);
+        $this->messenger()->addMessage($this->t('The selected video has been imported.'));
+      }
+      catch (\Exception $e) {
+        // Up until here, all necessary checks have been made. No custom exception
+        // handling needed other than for the db possibly exploding at this point.
+        $this->messenger()->addError($this->t('There has been an unexpected problem getting the video. Check the logs for details.'));
+        $this->logger->watchdogException($e);
+      }
     }
-
-    $updateRequest = UpdateVideoItemRequest::createFromMediaEntity($video_item);
-    try {
-      $this->updateVideoItemService->execute($updateRequest);
-      $this->messenger()->addMessage($this->t('The selected video has been updated.'));
-    }
-    catch (\Exception $e) {
-      // Up until here, all necessary checks have been made. No custom exception
-      // handling needed other than for the db possibly exploding at this point.
-      $this->messenger()->addError($this->t('There has been an unexpected problem updating the video. Check the logs for details.'));
-      $this->logger->watchdogException($e, 'mpx video with guid @guid could not be updated', ['@guid' => $guid]);
+    else {
+      $updateRequest = UpdateVideoItemRequest::createFromMediaEntity($video_item);
+      try {
+        $this->updateVideoItemService->execute($updateRequest);
+        $this->messenger()->addMessage($this->t('The selected video has been updated.'));
+      }
+      catch (\Exception $e) {
+        // Up until here, all necessary checks have been made. No custom exception
+        // handling needed other than for the db possibly exploding at this point.
+        $this->messenger()->addError($this->t('There has been an unexpected problem updating the video. Check the logs for details.'));
+        $this->logger->watchdogException($e, 'mpx video with guid @guid could not be updated', ['@guid' => $guid]);
+      }
     }
   }
 
@@ -167,6 +194,54 @@ class UpdateMediaItemForVideoType extends FormBase {
     }
 
     return $video;
+  }
+
+  /**
+   * Loads the media entity with a given guid and bundle from MPX.
+   *
+   * @param string $guid
+   *   The guid to filter by.
+   * @param string $type
+   *   The video type.
+   *
+   * @return \Drupal\media\Entity\Media|null
+   *   The mpx Media entity matching the given guid
+   */
+  private function loadVideoMatchingGuidAndTypeFromMpx(string $guid, string $type):?MpxMedia {
+    $media_type = $this->mpxTypeRepository->findByTypeId($type);
+    if (!$media_type) {
+      return NULL;
+    }
+    $mediaData = $this->fetchMediaTypeItemGuidFromMpx($media_type, $guid);
+    return $mediaData;
+  }
+
+  /**
+   * Fetches the item data from MPX for a given media type and GUID.
+   *
+   * @param \Drupal\media\MediaTypeInterface $media_type
+   *   The Media Type object.
+   * @param string $guid
+   *   The GUID of the media that want to retrieve.
+   *
+   * @return \Lullabot\Mpx\DataService\Media\Media
+   *   The Media entity returned by the mpx.
+   */
+  private function fetchMediaTypeItemGuidFromMpx(MediaTypeInterface $media_type, string $guid):?MpxMedia {
+    $field_query = new ByFields();
+    $field_query->addField('guid', $guid);
+    $query = new ObjectListQuery();
+    $query->add($field_query);
+
+    $media_source = DataObjectImporter::loadMediaSource($media_type);
+    $factory = $this->dataObjectFactoryCreator->fromMediaSource($media_source);
+
+    $results = $factory->select($query);
+    $queues = [];
+    foreach ($results as $index => $mpx_media) {
+      $queues[] = $mpx_media;
+    }
+    return reset($queues);
   }
 
   /**
