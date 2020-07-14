98e1a06cb58357e8c357d7540168a7e55edb19b0
Matthew Oliveira <m4olivei@gmail.com>
2020-07-14 15:43:31 -0400

    BO-723 Dependency injection for the availability filter

--- src/Plugin/views/filter/Availability.php
+++ src/Plugin/views/filter/Availability.php
@@ -2,10 +2,13 @@
 
 namespace Drupal\media_mpx\Plugin\views\filter;
 
+use Drupal\Component\Datetime\TimeInterface;
 use Drupal\Core\Database\Query\Condition;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Entity\Sql\SqlContentEntityStorage;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\views\Plugin\views\filter\FilterPluginBase;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Filter by mpx availability.
@@ -33,6 +36,53 @@ class Availability extends FilterPluginBase {
   protected $alwaysMultiple = TRUE;
 
   /**
+   * Entity type manager.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+  /**
+   * Date time service.
+   *
+   * @var \Drupal\Component\Datetime\TimeInterface
+   */
+  protected $time;
+
+  /**
+   * Constructs a Availability object.
+   *
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin_id for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
+   *   Entity type manager.
+   * @param \Drupal\Component\Datetime\TimeInterface $time
+   *   Date time service.
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, EntityTypeManagerInterface $entity_type_manager, TimeInterface $time) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->entityTypeManager = $entity_type_manager;
+    $this->time = $time;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('entity_type.manager'),
+      $container->get('datetime.time')
+    );
+  }
+
+  /**
    * {@inheritdoc}
    */
   protected function valueForm(&$form, FormStateInterface $form_state) {
@@ -56,14 +106,14 @@ class Availability extends FilterPluginBase {
     $this->ensureMyTable();
 
     /** @var \Drupal\media\MediaTypeInterface $media_type */
-    $media_type = \Drupal::entityTypeManager()->getStorage('media_type')->load($this->definition['media_type']);
+    $media_type = $this->entityTypeManager->getStorage('media_type')->load($this->definition['media_type']);
     $field_map = $media_type->getFieldMap();
     if (isset($field_map['Media:availableDate']) && isset($field_map['Media:expirationDate'])) {
-      $media_storage = \Drupal::entityTypeManager()->getStorage('media');
+      $media_storage = $this->entityTypeManager->getStorage('media');
       assert($media_storage instanceof SqlContentEntityStorage);
       $table_mapping = $media_storage->getTableMapping();
 
-      $field_storage = \Drupal::entityTypeManager()->getStorage('field_storage_config');
+      $field_storage = $this->entityTypeManager->getStorage('field_storage_config');
       /** @var \Drupal\Core\Field\FieldStorageDefinitionInterface $available_field_storage */
       $available_field_storage = $field_storage->load('media.' . $field_map['Media:availableDate']);
       /** @var \Drupal\Core\Field\FieldStorageDefinitionInterface $expired_field_storage */
@@ -73,11 +123,10 @@ class Availability extends FilterPluginBase {
       $this->query->ensureTable($available_table_name);
       $this->query->ensureTable($expired_table_name);
 
-      // @todo add a condition on the bundle.
+      // Add a condition on the bundle that this filter is defined for.
       $media_definition = \Drupal::entityTypeManager()->getDefinition('media');
       $this->query->addWhere($this->options['group'], sprintf('%s.%s', $media_storage->getDataTable(), $media_definition->getKey('bundle')), $this->definition['media_type']);
 
-      $time = \Drupal::time();
       $available_field = sprintf('%s.%s', $available_table_name, $table_mapping->getFieldColumnName($available_field_storage, 'value'));
       $expired_field = sprintf('%s.%s', $expired_table_name, $table_mapping->getFieldColumnName($expired_field_storage, 'value'));
       if (empty($this->value) || $this->value === self::AVAILABLE_UPCOMING) {
@@ -85,26 +134,26 @@ class Availability extends FilterPluginBase {
         $condition = (new Condition('OR'))
           ->condition($expired_field, 0)
           ->condition($expired_field, NULL, 'IS NULL')
-          ->condition($expired_field, $time->getRequestTime(), '>=');
+          ->condition($expired_field, $this->time->getRequestTime(), '>=');
         $this->query->addWhere($this->options['group'], $condition);
       }
       elseif ($this->value === self::AVAILABLE) {
         $available_condition = (new Condition('OR'))
           ->condition($available_field, 0)
           ->condition($available_field, NULL, 'IS NULL')
-          ->condition($available_field, $time->getRequestTime(), '<=');
+          ->condition($available_field, $this->time->getRequestTime(), '<=');
         $this->query->addWhere($this->options['group'], $available_condition);
         $expired_condition = (new Condition('OR'))
           ->condition($expired_field, 0)
           ->condition($expired_field, NULL, 'IS NULL')
-          ->condition($expired_field, $time->getRequestTime(), '>=');
+          ->condition($expired_field, $this->time->getRequestTime(), '>=');
         $this->query->addWhere($this->options['group'], $expired_condition);
       }
       elseif ($this->value === self::EXPIRED) {
         $condition = (new Condition('AND'))
           ->condition($expired_field, 0, '<>')
           ->condition($expired_field, NULL, 'IS NOT NULL')
-          ->condition($expired_field, $time->getRequestTime(), '<=');
+          ->condition($expired_field, $this->time->getRequestTime(), '<=');
         $this->query->addWhere($this->options['group'], $condition);
       }
     }
